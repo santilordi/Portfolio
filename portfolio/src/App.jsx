8164df6185353191a5d3c23236747cd3e600ad05
Santiago Lordi <santilordi569@gmail.com>
2026-05-06 00:33:38 -0300

    fix navbar bug

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 
 function App() {
 
   return (
-    <Router>
+    <>
       <Navbar/>
       <main style={{ paddingTop: '20px'}}>
-
+        
       </main>
-    </Router>
+    </>
   )
 }
 
