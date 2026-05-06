95e00979ea537734ac4329403d049d661ca7f8ad
Santiago Lordi <santilordi569@gmail.com>
2026-05-06 00:49:55 -0300

    define routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 
 function App() {
 
   return (
-    <>
+    <div>
       <Navbar/>
       <main style={{ paddingTop: '20px'}}>
-        
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/skills' element={<Skills/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
       </main>
-    </>
+    </div>
   )
 }
 
